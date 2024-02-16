1337e0c3b4ad35895b00c7be791c6afc9ea1ef49
Abhay Mishra <abhaymishra0902@gmail.com>
2024-02-16 16:03:36 +0530

    Initial commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_series/main.dart';
+import 'package:firebase_series/signuppage.dart';
+import 'package:firebase_series/uihelper.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  login(String email, String password) async {
+    if (email == "" && password == "") {
+      return UiHelper.CustomeAlertBox(context, "Enter Require Field");
+    } else {
+      UserCredential? usercredential;
+
+      try {
+        usercredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: email, password: password).then((value) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: "Myhomepage"))));
+      } on FirebaseAuthException catch (ex) {
+        return UiHelper.CustomeAlertBox(context, ex.code.toString());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+        centerTitle: true,
+        backgroundColor: Colors.black12,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UiHelper.CustomTextField(emailController, "Email", Icons.mail, false),
+          UiHelper.CustomTextField(
+              passwordController, "password", Icons.password, true),
+          SizedBox(
+            height: 30,
+          ),
+          UiHelper.CustomeButton(() {
+            login(emailController.text.toString(),
+                passwordController.text.toString());
+          }, "Login"),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an Account??",
+                style: TextStyle(fontSize: 16),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()));
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/signuppage.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_series/main.dart';
+import 'package:firebase_series/uihelper.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  SignUp(String email, String password) async {
+    if (email == "" && password == "") {
+      UiHelper.CustomeAlertBox(context, "Enter Require Field");
+    } else {
+      UserCredential? userCredential;
+      try {
+        userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MyHomePage(title: "HomePage",))));
+      } on FirebaseAuthException catch (ex) {
+        return UiHelper.CustomeAlertBox(context, ex.code.toString());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sign Up page"),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UiHelper.CustomTextField(
+                emailController, "Email", Icons.mail, false),
+            UiHelper.CustomTextField(
+                passwordController, "Password", Icons.password, true),
+            SizedBox(
+              height: 30,
+            ),
+            UiHelper.CustomeButton(() {
+              SignUp(emailController.text.toString(),
+                  passwordController.text.toString());
+            }, "Sign Up")
+          ],
+        ));
+  }
+}
